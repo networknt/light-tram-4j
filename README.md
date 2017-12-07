3e7783dbe9f7f897595015d79a8430813bc8b1ae
Steve Hu <stevehu@gmail.com>
2017-12-06 22:47:26 -0500

    add tram-cdc-mysql-server for restful CDC server

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # light-tram-4j
 Transactional Messaging framework for message/event/command driven interaction style
+
+### Build
+
+To build the project without running integration test cases. 
+
+```
+mvn clean package
+```
+
+If you have to install the jars into your local maven repository .m2, then you can
+
+```
+mvn clean install -DskipTests
+```
+
+Before running integration test cases during the build. We need to start Kafka, Zookeeper
+Mysql and CDC server. 
+
---
+++ tram-cdc-mysql-server/src/main/java/com/networknt/tram/cdc/server/CdcServer.java
@@ -0,0 +1,18 @@
+package com.networknt.tram.cdc.server;
+
+import com.networknt.server.HandlerProvider;
+import io.undertow.Handlers;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+
+/**
+ * CdcServer handle
+ */
+public class CdcServer implements HandlerProvider {
+    @Override
+    public HttpHandler getHandler() {
+        return Handlers.path()
+                .addPrefixPath("/", exchange -> exchange.getResponseSender().send("OK!")
+                );
+    }
+}
