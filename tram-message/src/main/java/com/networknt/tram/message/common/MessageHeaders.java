7a80a6a98b2995395ff3b97c8f4868d6103218fc
chenyan71 <passWord11!!>
2018-02-01 15:46:11 -0500

    new header constant object

---
+++ tram-message/src/main/java/com/networknt/tram/message/common/MessageHeaders.java
@@ -0,0 +1,7 @@
+package com.networknt.tram.message.common;
+
+public class MessageHeaders {
+  public static final String MESSAGE_TYPE = "message-type";
+  public static final String AGGREGATE_TYPE = "message-aggregate-type";
+  public static final String AGGREGATE_ID = "message-aggregate-id";
+}
